b6fb550d53d16867d1effb5713d44d97b72f6672
hyunsun <hyunstin7@gmail.com>
2025-12-24 11:04:46 +0900

    axios add

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,26 @@
-document.querySelectorAll('.nav-item').forEach(a=>{
-    a.addEventListener('mouseenter',(e)=>{
-        console.log(e.clientX);
-    })
+const boardList = document.getElementById('board-list');
+
+const getList = async () => {
+    try {
+        const res = await axios.get('http://localhost:8081/list')
+        const boards = res.data;
+        boards.forEach(board=>{
+            boardList.innerHTML += `
+            <tr>
+                <td>${board.b_idx}</td>
+                <td><a href="">${board.b_title}</a></td>
+                <td>${board.b_writer}</td>
+                <td>${board.b_datetime}</td>
+                <td>${board.b_count}</td>
+            </tr>
+            `
+        })
+
+    } catch (error) {
+            console.log(error);
+    }
+}
+
+document.addEventListener('DOMContentLoaded',()=>{
+    getList();
 })
